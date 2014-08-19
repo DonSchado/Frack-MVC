656270caba05af3f07d5ae5e566128508d0519f5
Marco Schaden <ms@donschado.de>
2014-08-19 10:21:35 +0200

    refactor application dispatching

--- lib/frack/application.rb
+++ lib/frack/application.rb
@@ -1,9 +1,19 @@
 module Frack
   class Application
     class << self
+      attr_accessor :env
+
       def call(env)
-        controller = Object.const_get(env['controller'].capitalize + 'Controller')
-        Rack::Response.new(controller.new.send(env['action']))
+        self.env = env
+        Rack::Response.new(*dispatch)
+      end
+
+      def dispatch
+        controller.new.public_send(env['action'])
+      end
+
+      def controller
+        Object.const_get(env['controller'].capitalize + 'Controller')
       end
     end
   end
